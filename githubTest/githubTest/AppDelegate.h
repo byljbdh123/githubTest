7d45dd5f04882b3a93022ec5185ae43e13909bad
byljbdh <alan@luojunbindeMacBook-Pro.local>
2015-08-11 20:30:43 +0800

    init

---
+++ githubTest/githubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubTest
+//
+//  Created by 罗骏彬 on 15/8/11.
+//  Copyright (c) 2015年 罗骏彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubTest/githubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubTest
+//
+//  Created by 罗骏彬 on 15/8/11.
+//  Copyright (c) 2015年 罗骏彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
